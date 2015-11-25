a77601552d6ce12d65705adc800288574a5a9415
Vitamin1899 <vitalimarinich@gmail.com>
2015-11-25 17:46:50 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Filespace::Application.config.secret_key_base = '523e19a175691cfa81ee0452d83808ad301ef679b2aec5cda5ee8be0471887becbbe8a38610f6bab42259b46c2defc74744d9224e2573d9db1067959720a7471'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Filespace::Application.config.session_store :cookie_store, key: '_filespace_session'
